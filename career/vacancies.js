6c8d31f8e22d389077c5a5da186423847e0a1aad
Jochi1973 <HenkBriel@outlook.com>
2024-02-02 10:56:44 +0200

    Vacancies JS File

---
+++ career/vacancies.js
@@ -0,0 +1,31 @@
+var opportunities = [
+    {
+        title: "HRD FACILITATOR: MINING (TMM) (CL)",
+        pdfLink: "31012024 Advertisement HRD Facilitator CL.pdf",
+        description: "HRD Facilitator is responsible for facilitating the transfer of specific job knowledge and skills to the Leaners, assessing the competency of the learners against National standards & performing all related administrative duties associated with the job.",
+        expiryDate: "2024-02-06"
+    },
+    {
+        title: "Foreman Electrical (C5)",
+        pdfLink: "25012024 Advertisement Foreman Electrical C5.pdf",
+        description: "To provide an electrical service and support to all production operations at the Middelpunt Hill mine through effective maintenance and repairs of equipment and machinery.",
+        expiryDate: "2024-01-31"
+    },
+    {
+        title: "PUMP ATTENDANT (B1)",
+        pdfLink: "25012024 Advertisement Pump Attendant B1.pdf",
+        description: "To safely attend to Pumps",
+        expiryDate: "2024-01-31"
+    }/*,
+            
+            entry template for future use
+            {
+              title: "",
+              pdfLink: "",
+              description: "",
+              expiryDate: "2023-11-24"
+            },
+
+
+            */
+];
