777dfc6a18c398ed5054b9ef21324646e2c4f985
Jochi1973 <HenkBriel@outlook.com>
2024-02-02 12:44:58 +0200

    New vacancy added

--- career/vacancies.js
+++ career/vacancies.js
@@ -4,35 +4,44 @@ var opportunities = [
         pdfLink: "31012024 Advertisement HRD Facilitator CL.pdf",
         description: "HRD Facilitator is responsible for facilitating the transfer of specific job knowledge and skills to the Leaners, assessing the competency of the learners against National standards & performing all related administrative duties associated with the job.",
         expiryDate: "2024-02-06"
-    },
+    }
+    ,
     {
         title: "Foreman Electrical (C5)",
         pdfLink: "25012024 Advertisement Foreman Electrical C5.pdf",
         description: "To provide an electrical service and support to all production operations at the Middelpunt Hill mine through effective maintenance and repairs of equipment and machinery.",
         expiryDate: "2024-01-31"
-    },
+    }
+    ,
     {
         title: "PUMP ATTENDANT (B1)",
         pdfLink: "25012024 Advertisement Pump Attendant B1.pdf",
         description: "To safely attend to Pumps",
         expiryDate: "2024-01-31"
-    },
+    }
+    ,
     {
         title: "Senior HR Officer (CU)",
         pdfLink: "23012024 Advertisement Senior HR Officer.pdf",
         description: "Responsible for all HR Generalist functions in respective areas of responsibility.",
         expiryDate: "2024-01-26"
+    },
+    {
+        title: "Senior HR Officer (CU)",
+        pdfLink: "25012024 Advertisement Foreman Electrical C5.pdf",
+        description: "Responsible for all HR Generalist functions in respective areas of responsibility.",
+        expiryDate: "2024-02-01"
     }
 
 
     /*,
-            
-            entry template for future use
-            ,{
+
+            ,
+            {
               title: "",
               pdfLink: "",
               description: "",
-              expiryDate: "2023-11-24"
+              expiryDate: "2024-02-02"
             }
 
 
