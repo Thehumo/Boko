5600411b57d79136a44b0a9e1a8cdd46db3bc300
Jochi1973 <HenkBriel@outlook.com>
2024-02-12 12:50:43 +0200

    News and Career Updates

--- career/vacancies.js
+++ career/vacancies.js
@@ -1,9 +1,9 @@
 var opportunities = [
     {
-        title: "HRD FACILITATOR: MINING (TMM) (CL)",
-        pdfLink: "31012024 Advertisement HRD Facilitator CL.pdf",
-        description: "HRD Facilitator is responsible for facilitating the transfer of specific job knowledge and skills to the Leaners, assessing the competency of the learners against National standards & performing all related administrative duties associated with the job.",
-        expiryDate: "2024-02-06"
+        title: "HRD INTERNSHIP OPPORTUNITIES",
+        pdfLink: "Bokoni_Platinum Mine_HRD Adverts Q1 2024 - Internship Opportunities",
+        description: "Bokoni Platinum Mine is offering opportunities to unemployed youth graduates in pursuit of gaining workplace exposure through Structured Development Plansatits Mine of the Future Mining Operation.",
+        expiryDate: "2024-02-22"
     }
     ,
     {
@@ -19,19 +19,7 @@ var opportunities = [
         description: "To safely attend to Pumps",
         expiryDate: "2024-01-31"
     }
-    ,
-    {
-        title: "Senior HR Officer (CU)",
-        pdfLink: "23012024 Advertisement Senior HR Officer.pdf",
-        description: "Responsible for all HR Generalist functions in respective areas of responsibility.",
-        expiryDate: "2024-01-26"
-    },
-    {
-        title: "Senior HR Officer (CU)",
-        pdfLink: "25012024 Advertisement Foreman Electrical C5.pdf",
-        description: "Responsible for all HR Generalist functions in respective areas of responsibility.",
-        expiryDate: "2024-02-01"
-    }
+    
 
 
     /*,
