caed21a3c4040a08bb9e5b52bd6c146a5559c536
Jochi1973 <HenkBriel@outlook.com>
2024-03-06 23:53:21 +0200

    New Vacancies

--- career/vacancies.js
+++ career/vacancies.js
@@ -1,16 +1,9 @@
 var opportunities = [
     {
-        title: "HRD INTERNSHIP OPPORTUNITIES",
-        pdfLink: "Bokoni_Platinum Mine_HRD Adverts Q1 2024 - Internship Opportunities.pdf",
+        title: "ARTISAN LEARNERSHIP OPPORTUNITIES",
+        pdfLink: "Bokoni_Platinum Mine - Artisan Learnership Opportunities.pdf",
         description: "Bokoni Platinum Mine is offering opportunities to unemployed youth graduates in pursuit of gaining workplace exposure through Structured Development Plansatits Mine of the Future Mining Operation.",
-        expiryDate: "2024-02-22"
-    }
-    ,
-    {
-        title: "Foreman Electrical (C5)",
-        pdfLink: "25012024 Advertisement Foreman Electrical C5.pdf",
-        description: "To provide an electrical service and support to all production operations at the Middelpunt Hill mine through effective maintenance and repairs of equipment and machinery.",
-        expiryDate: "2024-01-31"
+        expiryDate: "2024-03-12"
     }
     ,
     {
@@ -18,6 +11,83 @@ var opportunities = [
         pdfLink: "25012024 Advertisement Pump Attendant B1.pdf",
         description: "To safely attend to Pumps",
         expiryDate: "2024-03-31"
+    }
+    ,
+    {
+        title: "Artisan: Auto Electrician (C2)",
+        pdfLink: "05032024 Advertisement Artisan Auto Electricians C2.pdf",
+        description: "Assist the Foreman and Chargehand to deliver quality work output in the day-to-day work activities within the relevant policies, procedures and legislation.",
+        expiryDate: "2024-03-12"
+    }
+    ,
+    {
+        title: "Artisan Diesel Mechanic (C2)",
+        pdfLink: "05032024 Advertisement Artisan Diesel Mechanic C2.pdf",
+        description: "Assist the Foreman and Chargehand to deliver quality work output in the day-to-day work activities within the relevant policies, procedures and legislati",
+        expiryDate: "2024-03-12"
+    }
+    ,
+    {
+        title: "Artisan Fitter (C2)",
+        pdfLink: "05032024 Advertisement Artisan Fitter C2.pdf",
+        description: "To provide a mechanical service and support to all area operations through effective maintenance and installation of equipment and machinery.",
+        expiryDate: "2024-03-12"
+    }
+    ,
+    {
+        title: "Artisan HT Electrician",
+        pdfLink: "05032024 Advertisement Artisan HT Electrician.pdf",
+        description: "To provide electrical service and support to all area operations through effective maintenance and installation of electrical equipment.",
+        expiryDate: "2024-03-12"
+    }
+    ,
+    {
+        title: "Artisan Rigger (C2)",
+        pdfLink: "05032024 Advertisement Artisan Rigger C2.pdf",
+        description: "Assist the Foreman and deliver quality work output in the day-to-day work activities, within the relevant policies, procedures and legislation.",
+        expiryDate: "2024-03-12"
+    }
+    ,
+    {
+        title: "Changehouse Attendant (B1)",
+        pdfLink: "05032024 Advertisement Changehouse Attendant B1.pdf",
+        description: "To attend to the operations at the change house facilities and to maintain acceptable housekeeping in the facilities and the general area of responsibility.",
+        expiryDate: "2024-03-12"
+    }
+    ,
+    {
+        title: "Chargehand Mechanical (C3)",
+        pdfLink: "05032024 Advertisement Chargehand Mechanical C3.pdf",
+        description: "Assist the Foreman to deliver quality work output in the day-to-day work activities within the relevant policies, procedures and legislation.",
+        expiryDate: "2024-03-12"
+    }
+    ,
+    {
+        title: "Communications Coordinator (DL)",
+        pdfLink: "05032024 Advertisement Communications Coordinator DL.pdf",
+        description: "To formulate and execute the mines Communication strategy, which will foster effective information sharing between the business, internal and external stakeholders. To ensure effective communication is maintained as guided by the Communication strategy. To ensure the Company brand is visible. Custodian of events management for the mine.",
+        expiryDate: "2024-03-12"
+    }
+    ,
+    {
+        title: "Miner (C2)",
+        pdfLink: "05032024 Advertisement Miner C2.pdf",
+        description: "To effectively inspect and declare safe underground working areas, conduct vertical shaft examinations and lead barrel and structural repairs by ensuring safe working practices, continuous inspection of working areas and/or conditions, planning to achieve operational targets and monitoring the availability of resources. The miner has legal accountability for underground working areas in terms of the MHSA, will be required to contribute and assist the Engineering team with the execution of all the maintenance tasks. Most areas are in Care-and-Maintenance, but Ad-Hoc mining may occur whereby the position would also require the effectively operation of a stopping panel and/or development end in line with safe working practices and/or conditions.",
+        expiryDate: "2024-03-12"
+    }
+    ,
+    {
+        title: "Receptionist (B2)",
+        pdfLink: "05032024 Advertisement Receptionist B2.pdf",
+        description: "The Receptionist will be responsible for Managing the front reception desk at Bokoni Platinum Mines by screening calls, taking messages and managing visitors. Manage and Oversee the Reception and Office Administration within Bokoni Platinum Mines. The position will be transferring calls to the relevant departments as well as taking detailed messages and giving it to the relevant departments. Ordering all Office supplies and stationery.",
+        expiryDate: "2024-03-12"
+    }
+    ,
+    {
+        title: "UV Operator",
+        pdfLink: "05032024 Advertisement UV Operator.pdf",
+        description: "Assist and deliver quality work output in the day-to-day work activities, within the relevant policies, procedures and legislation.",
+        expiryDate: "2024-03-12"
     }
     
 
